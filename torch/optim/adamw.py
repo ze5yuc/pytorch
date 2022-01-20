6314222a618dc6797fa36a64c31309abb3b1615d
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-01-20 19:36:34 +0000

    [optim] update step in functional and pass state_steps instead of state
    ghstack-source-id: 02350726ace5c0c0e6ad8efe77953708da2eb0ae
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/71333

--- torch/optim/adamw.py
+++ torch/optim/adamw.py
@@ -122,7 +122,7 @@ class AdamW(Optimizer):
 
                 # State initialization
                 if len(state) == 0:
-                    state['step'] = 0
+                    state['step'] = torch.tensor(0.)
                     # Exponential moving average of gradient values
                     state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
                     # Exponential moving average of squared gradient values
@@ -137,9 +137,6 @@ class AdamW(Optimizer):
                 if amsgrad:
                     max_exp_avg_sqs.append(state['max_exp_avg_sq'])
 
-                # update the steps for each param group update
-                state['step'] += 1
-                # record the step after step update
                 state_steps.append(state['step'])
 
             F.adamw(params_with_grad,
