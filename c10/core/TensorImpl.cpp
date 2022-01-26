bf312c2ff7608a0b16a3681e2a0ab9040d7939af
Brian Hirsh <hirsheybar@fb.com>
2022-01-26 08:13:01 -0800

    free up dispatch key space (in C++)
    ghstack-source-id: f7439cd0c3eda1fee5577258875122fa93930ce8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69633

--- c10/core/TensorImpl.cpp
+++ c10/core/TensorImpl.cpp
@@ -145,8 +145,7 @@ TensorImpl::TensorImpl(
       numel_(0),
       data_type_(data_type),
       device_opt_(storage_.device()),
-      key_set_(key_set.remove(
-          DispatchKey::Python)) { // See [Note: Python key removal]
+      key_set_(key_set.remove(DispatchKey::Python)) { // See [Note: Python key removal]
   init_bitfields();
   // Inference tensor doesn't have version counter.
   if (!is_inference()) {
@@ -187,7 +186,7 @@ TensorImpl::TensorImpl(
 
   // TODO: be more explicit about the full key set at call sites so we
   // don't have to keep recomputing it here
-  DispatchKey k = key_set.highestPriorityBackendTypeId();
+  auto k = key_set.highestBackendKey();
 
   key_set = key_set | getAutocastRelatedKeySetFromBackend(k);
 
